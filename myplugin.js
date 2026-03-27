982822b394ea7a03bdd483cd87912917bc0ebbbd
matsumoto-h-spec <matsumoto-h@azusasekkei.co.jp>
2026-03-27 16:44:26 +0900

    Update myplugin.js

--- myplugin.js
+++ myplugin.js
@@ -1,3 +1,17 @@
 Draw.loadPlugin(function(ui) {
-  alert('テスト2');
+  try {
+    var model = ui.editor.graph.getModel();
+    var cellIds = [];
+    
+    for (var key in model.cells) {
+      var cell = model.cells[key];
+      if (cell && cell.id) {
+        cellIds.push(cell.id);
+      }
+    }
+    
+    alert('全セル数: ' + cellIds.length + '個\nセルID: ' + cellIds.slice(0, 10).join(', '));
+  } catch(e) {
+    alert('エラー: ' + e.message);
+  }
 });
