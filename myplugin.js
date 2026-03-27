355d85d4aa6e3d1c5aa53535ce8b849ca7ce4997
matsumoto-h-spec <matsumoto-h@azusasekkei.co.jp>
2026-03-27 16:41:51 +0900

    Update myplugin.js

--- myplugin.js
+++ myplugin.js
@@ -1,29 +1,17 @@
 Draw.loadPlugin(function(ui) {
-  console.log('✅ プラグイン読み込み成功');
-  
-  var model = ui.editor.graph.getModel();
-  var cellIds = [];
-  
-  for (var key in model.cells) {
-    var cell = model.cells[key];
-    if (cell && cell.id) {
-      cellIds.push(cell.id);
+  try {
+    var model = ui.editor.graph.getModel();
+    var cellIds = [];
+    
+    for (var key in model.cells) {
+      var cell = model.cells[key];
+      if (cell && cell.id) {
+        cellIds.push(cell.id);
+      }
     }
-  }
-  
-  console.log('全セルID (' + cellIds.length + '個): ' + cellIds.join(', '));
-  
-  // URLからセルIDを取得
-  var hash = window.location.hash;
-  var match = hash.match(/cellId=([^&]+)/);
-  var targetId = match ? match[1] : null;
-  
-  if (targetId) {
-    var cell = model.getCell(targetId);
-    if (cell) {
-      ui.editor.graph.setSelectionCell(cell);
-      ui.editor.graph.scrollCellToVisible(cell, true);
-      console.log('✅ セル ' + targetId + ' を選択しました');
-    }
+    
+    alert('全セル数: ' + cellIds.length + '個\nセルID: ' + cellIds.slice(0, 10).join(', '));
+  } catch(e) {
+    alert('エラー: ' + e.message);
   }
 });
