2f7ade62878d9cb31efdde3c1db0cb17d7a2e327
matsumoto-h-spec <matsumoto-h@azusasekkei.co.jp>
2026-03-30 10:50:11 +0900

    Update myplugin.js

--- myplugin.js
+++ myplugin.js
@@ -1,31 +1,32 @@
 Draw.loadPlugin(function(ui) {
     var graph = ui.editor.graph;
 
-    // 図面が読み込まれたら実行
+    // ファイル読み込み完了時に実行
     ui.editor.addListener('fileLoaded', function() {
-        // 1. URLから「target」というパラメータを探してIDを読み取る
+        // 1. URLの「target」という文字の後ろを読み取る
         var urlParams = new URLSearchParams(window.location.hash.replace('#', '?'));
-        var id = urlParams.get('target'); 
+        var targetName = urlParams.get('target'); 
 
-        if (id) {
-            // 2. graph.getModel().getCell(id) でセルを取得
-            var cell = graph.getModel().getCell(id);
+        if (targetName) {
+            var model = graph.getModel();
+            var foundCell = null;
 
-            if (cell) {
-                // 3. graph.setSelectionCell(cell) で選択
-                graph.setSelectionCell(cell);
-                // 4. graph.scrollCellToVisible(cell, true) でズーム
-                graph.scrollCellToVisible(cell, true);
+            // 2. 140個の中から「名前」が一致するセルを探して取得
+            for (var id in model.cells) {
+                var cell = model.cells[id];
+                var label = graph.getLabel(cell).replace(/<[^>]*>/g, "").trim();
+                if (label === targetName) {
+                    foundCell = cell;
+                    break;
+                }
             }
-        }
-    });
 
-    // 【おまけ機能】140個のIDを調べるのが大変なので、
-    // 図形を「Ctrl + クリック」したらそのIDを表示するようにします
-    graph.addListener(mxEvent.CLICK, function(sender, evt) {
-        var cell = evt.getProperty('cell');
-        if (cell != null && evt.getEvent().ctrlKey) {
-            alert('この図形のIDは: ' + cell.id);
+            if (foundCell) {
+                // 3. graph.setSelectionCell(cell) で選択
+                graph.setSelectionCell(foundCell);
+                // 4. graph.scrollCellToVisible(cell, true) でズーム
+                graph.scrollCellToVisible(foundCell, true);
+            }
         }
     });
 });
